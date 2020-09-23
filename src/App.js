7782c68b6bff8fc6f1c5d602a47e567a876dd25d
kjy31056-creator <kjy31056@gamil.com>
2020-09-23 15:27:45 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Customer from './components/Customer';
+
+const customers= [
+  {
+  'id' : '1',
+  'img' : 'http://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday' : '910326' ,
+  'gender': '남',
+  'job' : '대학생' 
+  },
+  {
+  'id' : '2',
+  'img' : 'http://placeimg.com/64/64/2',
+  'name' : '임꺽정',
+  'birthday' : '920408' ,
+  'gender': '남',
+  'job' : '프로그레머' 
+  },
+  {
+    'id' : '3',
+    'img' : 'http://placeimg.com/64/64/3',
+    'name' : '아무개',
+    'birthday' : '961010' ,
+    'gender': '남',
+    'job' : '디자이너' 
+  }
+
+]
+
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="logo"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+       {customers.map(c => {
+         return(
+          <Customer
+            key={c.id}
+            id={c.id}
+            img={c.img}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}/>
+          );
+        }
+      )}
     </div>
+  
   );
 }
 
